e3a385aad6f9850d69dd0f9a01f1f38922af1108
ChesterSzatkowski <89552700+ChesterSzatkowski@users.noreply.github.com>
2021-12-05 17:56:19 -0700

    Trying to get simulation to behave as it should

--- RoboCode.m
+++ RoboCode.m
@@ -7,17 +7,23 @@ clear all;
 clc;
 %% Numerical Solving
 species = 7;
-tspan = [0 200]; %seconds
-x0 = [0.1, 0, 0.65, 0, 0, 0, 0]; %initials
+tspan = [0 1000]; %seconds
+x0 = [0.33, % initial robot population fraction
+    0.33, % initial small object population fraction
+    0.33, % initial large object population fraction
+    0, % initial large transportation population fraction
+    0, % initial small transportation population fraction
+    0, % initial delivered large object population fraction
+    0]; % initial delivered small object population fraction
 
 M = readmatrix('M.txt');
 
-alpha1 = 2; % rate for robots working to move a large object into transportation phase
-alpha2 = 1; % rate for robot working to move a small object into transportation phase
-alpha3 = 0.5; % rate for large object to be delivered and 4 robots to be freed
-alpha4 = 0.5; % rate for small object to be delivered and a robot to be freed
-alpha5 = 0.8; % rate for a robot to take 10 small objects to make a large object
-beta = 0.0; % rate for robot to disassemble a large object into 10 small objects
+alpha1 = 0.02; % rate for robots working to move a large object into transportation phase
+alpha2 = 0.01; % rate for robot working to move a small object into transportation phase
+alpha3 = 0.01; % rate for large object to be delivered and 4 robots to be freed
+alpha4 = 0.01; % rate for small object to be delivered and a robot to be freed
+alpha5 = 0.003; % rate for a robot to take 10 small objects to make a large object
+beta = 0.001; % rate for robot to disassemble a large object into 10 small objects
 
 K = [alpha1, 0, 0, 0, 0, 0, 0, 0, 0, 0;
     -alpha1, 0, 0, 0, 0, 0, 0, 0, 0, 0;
@@ -29,7 +35,6 @@ K = [alpha1, 0, 0, 0, 0, 0, 0, 0, 0, 0;
     0, 0, 0, 0, 0, 0, -alpha4, 0, 0, 0;
     0, 0, 0, 0, 0, 0, 0, 0, alpha5, -beta;
     0, 0, 0, 0, 0, 0, 0, 0, -alpha5, beta];
-
 
 [t,x] = ode45(@(t,x) odefun(t,x,M,K), tspan, x0);
 hold on;
@@ -71,7 +76,7 @@ V = @Vfn;
 end
 function dxdt = odefun(t,x,M,K)
 % x = [x(1); x(2); x(3); x(4); x(5); x(6); x(7)];
-y = [4*x(1)*x(3); x(4); x(1)*x(2); x(5); x(4); 4*x(1)*x(6); x(5); x(7)*x(1); 10*x(2)*x(1); x(3)];
+y = [4*x(1)*x(3); x(4); x(1)*x(2); x(5); x(4); 4*x(1)*x(6); x(5); x(7)*x(1); 10*x(2)*x(1); x(3)*x(1)];
 
 dxdt = -M*K*y;
 end
